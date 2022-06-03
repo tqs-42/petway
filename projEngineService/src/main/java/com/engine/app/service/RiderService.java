5a4478ce7e653ece307d779f406c91b1b33216b7
TheScorpoi <sobral@ua.pt>
2022-06-03 17:45:11 +0100

    remove unused person repo

--- projEngineService/src/main/java/com/engine/app/service/RiderService.java
+++ projEngineService/src/main/java/com/engine/app/service/RiderService.java
@@ -22,8 +22,6 @@ import java.util.List;
 @Service
 public class RiderService {
 
-    @Autowired
-    private PersonRepository personRepository;
 
     @Autowired
     private RiderRepository riderRepository;
