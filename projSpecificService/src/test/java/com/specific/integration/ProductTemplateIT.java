96f248def716356557a52cc167731891eaf09c95
daniff15 <dani.fig@ua.pt>
2022-06-23 05:14:59 +0100

    integration tests product

--- projSpecificService/src/test/java/com/specific/integration/ProductTemplateIT.java
+++ projSpecificService/src/test/java/com/specific/integration/ProductTemplateIT.java
@@ -16,12 +16,14 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.springframework.http.*;
 import com.github.dockerjava.api.exception.ConflictException;
+import com.specific.controller.ProductController;
 import com.specific.model.Category;
 import com.specific.model.Product;
 import com.specific.model.Store;
 import com.specific.repository.CategoryRepository;
 import com.specific.repository.ProductRepository;
 import com.specific.repository.StoreRepository;
+import com.specific.service.ProductService;
 
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 import org.testcontainers.shaded.com.fasterxml.jackson.core.type.TypeReference;
@@ -65,7 +67,7 @@ class ProductTemplateIT {
         registry.add("spring.datasource.username", container::getUsername);
     }
 
-    private Product product;
+    private String product;
     private Store store;
     private Category category;
 
@@ -77,98 +79,69 @@ class ProductTemplateIT {
         this.category.setName("Categoria");
         this.store.setName("Loja");
         this.store.setAddress("A minha morada");
-        this.product = new Product("Produtinho", "description", "image", 15.0, 1, this.category, this.store);
 
         categoryRepository.saveAndFlush(this.category);
         storeRepository.saveAndFlush(this.store);
 
-    }
-
-    @AfterEach
-    void resetDb() {
+        this.product = "{\"name\": \"PetyCity\", \"description\": \"Rua P\", \"image\": \"vera@ua.pt\", \"price\": 2.4, \"stock\": 5, \"store\":1, \"category\":1}";
     }
 
     @Test
     void testAddProduct_thenCode200() {
-        this.category = new Category();
-        this.store = new Store();
 
-        this.category.setName("Categoria");
-        this.store.setName("Loja");
-        this.store.setAddress("A minha morada");
+        RestAssured.given().contentType("application/json").body(this.product).when().post(getBaseUrl() + "/add")
+                .then()
+                .statusCode(200)
+                .and().body("name", equalTo("PetyCity"))
+                .and().body("description", equalTo("Rua P"))
+                .and().body("image", equalTo("vera@ua.pt"))
+                .and().body("price", equalTo(2.4F))
+                .and().body("stock", equalTo(5));
+
+    }
+
+    @Test
+    void testProductAlreadyExists_thenReturnConflit() throws ConflictException
+
+    {
+        String sameNameProduct = "{\"name\": \"PetyCity\", \"description\": \"Outra descricao\", \"image\": \"vera@ua.pt\", \"price\": 2.4, \"stock\": 5, \"store\":1, \"category\":1}";
 
         RestAssured.given()
                 .contentType("application/json")
-                .body(this.product)
+                .body(sameNameProduct)
+                .when()
+                .post(getBaseUrl() + "/add")
+                .then()
+                .statusCode(409);
+
+    }
+
+    @Test
+    public void testWhenGetById_thenOKRequest() {
+        String sameNameProduct = "{\"name\": \"AnotherCity\", \"description\": \"Outra descricao\", \"image\": \"imagem\", \"price\": 2.5, \"stock\": 55, \"store\":1, \"category\":1}";
+
+        RestAssured.given()
+                .contentType("application/json")
+                .body(sameNameProduct)
                 .when()
                 .post(getBaseUrl() + "/add")
                 .then()
                 .statusCode(200);
 
-        // HttpHeaders headers = new HttpHeaders();
-        // headers.setContentType(MediaType.APPLICATION_JSON);
+        RestAssured.given()
+                .contentType("application/json")
+                .body(sameNameProduct)
+                .when()
+                .get(getBaseUrl() + "/1")
+                .then()
+                .statusCode(200)
+                .and().body("name", equalTo("AnotherCity"))
+                .and().body("description", equalTo("Outra descricao"))
+                .and().body("image", equalTo("imagem"))
+                .and().body("price", equalTo(2.5F))
+                .and().body("stock", equalTo(55));
 
-        // ResponseEntity<Product> response = testRestTemplate.exchange(
-        // getBaseUrl() + "/add", HttpMethod.POST, new HttpEntity<>(product, headers),
-        // Product.class);
-
-        System.out.println("FDGP");
-
-        // System.out.println(response.getStatusCode());
-
-        // HttpHeaders headers = new HttpHeaders();
-        // headers.setContentType(MediaType.APPLICATION_JSON);
-
-        // ResponseEntity<Product> response = testRestTemplate.exchange(
-        // getBaseUrl() + "/add", HttpMethod.POST, new HttpEntity<>(p1, headers),
-        // Product.class);
-
-        // assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
-
-    // @Test
-    // void testCategoryAlreadyExists_thenReturnConflit() throws ConflictException
-
-    // {
-    // categoryRepository.saveAndFlush(new Category("Food"));
-
-    // Category sameCategory = new Category("Food");
-
-    // RestAssured.given()
-    // .contentType("application/json")
-    // .body(sameCategory)
-    // .when()
-    // .post(getBaseUrl() + "/add")
-    // .then()
-    // .statusCode(409);
-
-    // }
-
-    // @Test
-    // public void testWhenAllCategory_thenOKRequest() {
-    // HttpHeaders headers = new HttpHeaders();
-    // ResponseEntity<List> response = testRestTemplate.exchange(
-    // getBaseUrl(), HttpMethod.GET, new HttpEntity<Object>(headers),
-    // List.class);
-
-    // assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-
-    // List<Category> found = response.getBody();
-
-    // List<Category> categories = new ArrayList<>();
-
-    // for (Object category : found) {
-    // String categoria = String.valueOf(category);
-    // String id_str = categoria.split(",")[0].split("=")[1];
-    // long id = Long.parseLong(id_str);
-    // String name = categoria.split(",")[1].split("=")[1].replace("}", "");
-
-    // categories.add(new Category(id, name));
-    // }
-
-    // assertThat(categories.get(0).getName()).contains(category1.getName());
-    // assertThat(categories.get(1).getName()).contains(category2.getName());
-    // }
 
     String getBaseUrl() {
         return "http://localhost:" + randomServerPort + "/products";
